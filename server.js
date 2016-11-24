2043d7308e3a18cf47d59fa00d3765bc549a1b1f
sasikumar13 <sasi.13kumar07@gmail.com>
2016-11-24 15:16:00 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
+var Pool = require('pg').Pool;
+ 
+ var config = {
+    host: 'db.imad.hasura-app.io',
+    user: 'sasikumar13',
+    port: '5432', 
+    database: 'sasikumar13',
+    password: process.env.DB-PASSWORD
+};
+ 
+ 
 var app = express();
 app.use(morgan('combined'));
 
 var articleOne = {
     
-    title: 'Article One | Sasi'
-    heading: 'Article One'
-    date: 'Oct 1, 2016'
+    title: 'Article One | Sasi',
+    heading: 'Article One',
+    date: 'Oct 1, 2016',
     content: `
         <p>
             This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
@@ -21,6 +31,7 @@ var articleOne = {
             This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.This is the content for my first article.
         </p>`
 }
+
 function createTemplate (data){
     var title = data.title;
     var date= data.date;
@@ -57,12 +68,27 @@ function createTemplate (data){
     `;
     return htmlTemplate;
 }
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+var pool = new Pool(config)
+app.get('/test-db',function (req, res) {
+    pool.query('SELECT * FROM test', function(err,result){ 
+        if(err) {
+            res.status(500).send(err.toString());
+        } else {
+            res.send(JSON.stringify(result));
+        }   
+    
+    });
+    
+}); 
+
 app.get('/article-one',function (req, res){
     res.send(createTemplate(articleOne));
+});
 
 app.get('/article-two',function (req, res){
      res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
