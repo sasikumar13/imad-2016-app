3c3750a34cd6b3c1827cf6f1b12b2e77c6bd6cb2
sasikumar13 <sasi.13kumar07@gmail.com>
2016-11-24 15:57:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -101,10 +101,20 @@ app.get('/submit-name', function(req,res) {
 });
 
 app.get('/article /article-one',function (req, res){
-    var articleName = req.params.articleName;   
-    res.send(createTemplate(articles[articleName]));
+   
+    pool.query("SELECT * FROM article WHERE title =" + req.params.articleName,function(err, result) {
+        if (err) {
+            res.status(500).send(err.toString());
+        } else {
+            if (result.rows.length === 0){
+                res.status(404).send('Article Not found');
+            } else { 
+                var articleData = result.rows[0];
+                 res.send(createTemplate(articleData));
+            }
+        }
+    });
 });
-
 
 
 app.get('/ui/style.css', function (req, res) {
